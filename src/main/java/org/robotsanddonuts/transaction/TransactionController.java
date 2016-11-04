b155d27be1aeeeba8c48c41205f74175e7c0eee4
Paul O'Connor <jamespauloconnor@gmail.com>
2016-11-04 09:56:23 -0400

    Stubbed out transactions controller and views

--- src/main/java/org/robotsanddonuts/transaction/TransactionController.java
+++ src/main/java/org/robotsanddonuts/transaction/TransactionController.java
@@ -1,5 +1,11 @@
 package org.robotsanddonuts.transaction;
 
+import java.sql.SQLException;
+
+import javax.validation.Valid;
+
+import org.robotsanddonuts.beans.Customer;
+import org.robotsanddonuts.beans.Product;
 import org.robotsanddonuts.beans.Transaction;
 import org.robotsanddonuts.repository.CustomerRepository;
 import org.robotsanddonuts.repository.ProductRepository;
@@ -7,21 +13,38 @@ import org.robotsanddonuts.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TransactionController {
-	
+
 	@Autowired
 	private TransactionRepository transactionRepo;
+
+	@Autowired
+	private CustomerRepository customerRepo;
 	
 	@Autowired
 	private ProductRepository productRepo;
 	
-	@Autowired
-	private CustomerRepository customerRepo;
+	@GetMapping("/transactions")
+	public String findAllTransactions(Model model) throws SQLException {
+		model.addAttribute("transactions", transactionRepo.findAll());
+		return "transaction/transactions";
+	}
+	/*
+	 * CREATE METHODS
+	 */
 
+	
+	
+	/*
+	 * Edit Methods
+	 */
 	@GetMapping("/transaction/{id}/edit")
 	public String transactionEdit(Model model, @PathVariable(name = "id") long id) {
 		model.addAttribute("id", id);
@@ -31,4 +54,16 @@ public class TransactionController {
 		model.addAttribute("products", productRepo.findAll());
 		return "transaction/transaction_edit";
 	}
+	
+
+	/*
+	 * READ Methods
+	 */
+	
+	
+
+	/*
+	 * DELETE Methods
+	 */
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,6 @@ insert into robotsanddonuts.products (upc, product_name, product_manufacturer, p
 insert into robotsanddonuts.products (upc, product_name, product_manufacturer, product_price) values ('d98f8c8gg', 'BaconRumblers', 'Titus', 9.99);
 insert into robotsanddonuts.products (upc, product_name, product_manufacturer, product_price) values ('d98f8c8gg', 'Jelly Filled', 'Titus', 9.99);
 insert into robotsanddonuts.products (upc, product_name, product_manufacturer, product_price) values ('d98f8c8gg', 'Stuff', 'Titus', 9.99);
-insert into robotsanddonuts.products (upc, product_name, product_manufacturer, product_price) values ('d98f8c8gg', 'Stuff', 'Titus', 9.99);
 
 insert into robotsanddonuts.customers (first_name, last_name, email, phone_number) values ('John', 'Bon Jovi', 'jon@bonjovi.com','999-999-9999');
 insert into robotsanddonuts.customers (first_name, last_name, email, phone_number) values ('John', 'Bon Jovi', 'jon@bonjovi.com','999-999-9999');
